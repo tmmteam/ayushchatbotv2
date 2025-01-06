c225fca37be9d5d345ef4d0e3e0a8c85a1f732e6
tmmteam <teamtmm2@gmail.com>
2025-01-06 12:58:22 +0530

    Update inline.py

--- Ayush/modules/helpers/inline.py
+++ Ayush/modules/helpers/inline.py
@@ -107,7 +107,7 @@ HELP_START = [
 HELP_BUTN = [
     [
         InlineKeyboardButton(
-            text="« ʜᴇʟᴘ »", url=f"https://t.me/{AarohiX.username}?start=help"
+            text="« ʜᴇʟᴘ »", url=f"https://t.me/{Ayush.username}?start=help"
         ),
         InlineKeyboardButton(text="⦿ ᴄʟᴏsᴇ ⦿", callback_data="CLOSE"),
     ],
