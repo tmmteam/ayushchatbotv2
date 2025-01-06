547ba5fa83256db089b1168aec3d1cca485de960
tmmteam <teamtmm2@gmail.com>
2025-01-06 12:43:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6481884068"
-SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
+SUPPORT_GRP = "tmm_support_chat"
 UPDATE_CHNL = "tmm_heroku_world"
 OWNER_USERNAME = "expertcoderr"
